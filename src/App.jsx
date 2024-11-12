0235c7a3b58fa6422bcc524107863a90d3158268
Diego <diegosebastianorona@gmail.com>
2024-11-12 19:04:44 -0300

    Componentizando Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { ChakraProvider } from '@chakra-ui/icons'
+import { ChakraProvider, Flex } from '@chakra-ui/icons'
 import ItemListContainer from './assets/components/ItemListContainer/ItemListCointainer'
 import NavBar from './assets/components/NavBar/NavBar'
+import { React } from 'react'
+import Counter from './assets/components/Counter/Counter'
+
+
+
 function App() {
   
+
+
 
   return (
     <>
     <ChakraProvider>
     <NavBar/>
     <ItemListContainer greeting="Saludos Terricola"/>
+    <Flex alignItems={"center"} textAlign={"center"} justifyContent={"center"}>
+      <Counter/>
+    </Flex>
     </ChakraProvider>
     </>
   )

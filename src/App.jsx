35849766807e1f94ff7912942fd769ddde6302ab
Diego <diegosebastianorona@gmail.com>
2024-11-12 20:25:39 -0300

    Agregando Layaut y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { ChakraProvider, Flex } from '@chakra-ui/icons'
 import ItemListContainer from './assets/components/ItemListContainer/ItemListCointainer'
-import NavBar from './assets/components/NavBar/NavBar'
 import { React } from 'react'
 import Counter from './assets/components/Counter/Counter'
+import Layout from './assets/layouts/Layout'
 
 
 
@@ -14,11 +14,12 @@ function App() {
   return (
     <>
     <ChakraProvider>
-    <NavBar/>
-    <ItemListContainer greeting="Saludos Terricola"/>
-    <Flex alignItems={"center"} textAlign={"center"} justifyContent={"center"}>
-      <Counter/>
-    </Flex>
+    <Layout>
+      <ItemListContainer greeting="Saludos Terricola"/>
+      <Flex alignItems={"center"} textAlign={"center"} justifyContent={"center"}>
+        <Counter/>
+      </Flex>
+    </Layout>
     </ChakraProvider>
     </>
   )

c48dcfa68207f2a34ffd9d7b4f06cd8c01ab2fe2
Diego <diegosebastianorona@gmail.com>
2024-12-03 21:31:24 -0300

    refactorizando codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { ChakraProvider, Flex } from '@chakra-ui/icons'
+import { ChakraProvider } from '@chakra-ui/icons'
 import { React } from 'react'
-import Counter from './assets/components/Counter/Counter'
 import Layout from './assets/layouts/Layout'
 import Home from './assets/pages/Home'
-
 
 
 function App() {
@@ -12,9 +10,6 @@ function App() {
     <ChakraProvider>
     <Layout>
       <Home/>
-      <Flex alignItems={"center"} textAlign={"center"} justifyContent={"center"}>
-        <Counter/>
-      </Flex>
     </Layout>
     </ChakraProvider>
     </>
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,21 +1,27 @@
-import { Flex, Spinner } from "@chakra-ui/react";
-import ItemListContainer from '../components/ItemListContainer/ItemListCointainer'
-import { useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import ItemListContainer from "../components/ItemListContainer/ItemListCointainer";
+import Loader from "../components/Loader/Loader";
+import { useGetAllProducts } from "../hooks/useGetAllProducts";
 
 
-const Home = ()=>{
-    const [loading, setLoading]= useState(true)
 
-    setTimeout(()=>{
-        setLoading(false)
-    },2000);
+const Home = () => {
+    
+    const {loading, products} = useGetAllProducts();
+
+
+    // setTimeout(() => {
+    //     setLoading(false)
+    // }, 2000)
+
 
     return loading ? (
-        <Flex alignItems={"center"} justifyContent={"center"} height={"90vh"}>
-            <Spinner/>
-        </Flex>
+        <Loader />
     ) : (
-        <ItemListContainer greeting="Saludos Terricola"/>
+        <Flex alignItems={"center"} textAlign={"center"} justifyContent={"center"}>
+            <ItemListContainer pro={products} />
+        </Flex>
     );
+
 }
 export default Home;

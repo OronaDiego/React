5ac9a9c2f8adf454419d8c4c3e2d30f9b2f68427
Diego <109989239+OronaDiego@users.noreply.github.com>
2025-01-08 21:24:06 -0300

    Update

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
-export async function getAllProducts(limit = 30) {
-  return await axios.get(`https://dummyjson.com/products?limit=${limit}`);
-}
+// export async function getAllProducts(limit = 30) {
+//   return await axios.get(`https://dummyjson.com/products?limit=${limit}`);
+// }
 
 // export async function getAllCategories() {
 //   return await axios.get("https://dummyjson.com/products/categories");
@@ -12,6 +12,6 @@ export async function getAllProducts(limit = 30) {
 //   return await axios.get(`https://dummyjson.com/products/${id}`);
 // }
 
-export async function getProductsByCategory(id) {
-  return await axios.get(`https://dummyjson.com/products/category/${id}`);
-}
+// export async function getProductsByCategory(id) {
+//   return await axios.get(`https://dummyjson.com/products/category/${id}`);
+// }
